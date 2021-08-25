aed8140e90824ff360350d75c49e77bc3862e4e0
adam <adam.zurada@wmb.expert>
2021-08-25 11:39:25 +0200

    added git ignore configuration

---
+++ src/test/java/com/example/kafkastreamsdemoconfig/KafkaStreamsDemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamsdemoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsDemoConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
