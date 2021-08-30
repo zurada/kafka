34141d6b8a0f2aff9ff69a07b40199f6107d43e2
adam <adam.zurada@wmb.expert>
2021-08-30 12:10:14 +0200

    added basic configuration for streams

--- src/main/java/com/example/kafkastreamsdemoconfig/KafkaStreamsDemoConfigApplication.java
+++ src/main/java/com/example/kafkastreamsdemoconfig/KafkaStreamsDemoConfigApplication.java
@@ -1,13 +1,25 @@
 package com.example.kafkastreamsdemoconfig;
 
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class KafkaStreamsDemoConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaStreamsDemoConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsDemoConfigApplication.class, args);
+    }
 
+    @Bean
+    public Consumer<KStream<Object, String>> process() {
+
+        return input ->
+                input.foreach((key, value) -> {
+                    System.out.println("Key: " + key + " Value: " + value);
+                });
+    }
 }
